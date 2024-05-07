785f167783c548d3223b02940eac0a5fb962ef28
Agostinho Ferreira <agostinho008.af45@gmail.com>
2024-05-07 20:56:28 -0300

    finalizado

--- DevConverter/script.js
+++ DevConverter/script.js
@@ -17,4 +17,33 @@ function handleSubmit(e){
     else if(!selectedCurrency.value){
         alert('Escolha uma moeda!');
     }
+    converter();
+    
+}
+function converter(){
+    if(selectedCurrency.value === 'eur'){
+        valueConverted = inputValue.value / 6.22;
+        result.innerHTML = valueFormatter('pt-BR', 'EUR');
+        animateResult();
+    }
+    else if(selectedCurrency.value === 'dol'){
+        valueConverted = inputValue.value / 5.37;
+        result.innerHTML = valueFormatter('en-US', 'USD');
+        animateResult();
+    }
+    inputValue.value = '';
+    selectedCurrency.value = '';
+}
+function valueFormatter(locate, currency){
+    const value = valueConverted.toLocaleString(`${locate}`, {style: 'currency', currency:`${currency}`});
+    return `<span>💰</span>${value}<span>💰</span>`
+}
+function animateResult(){
+    return result.animate(
+        [
+            {transform: 'translateY(-100px)'},
+            {transform: 'translateY(0px)'},
+        ],
+        { duration: 400 }
+    )
 }
